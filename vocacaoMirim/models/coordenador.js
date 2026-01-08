6e430f7acaded8d651eee6c117ae41959ba987ae
Alyne001 <alyne.souza62@aluno.ifce.edu.br>
2026-01-08 14:30:59 -0300

    Site Vocação Mirim PWEB1

--- vocacaoMirim/models/coordenador.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CoordenadorSchema = new mongoose.Schema({
-  nome: String,
-  capela: String,
-  senha: String
-});
-
-module.exports = mongoose.model("coordenadores", CoordenadorSchema);
